c25abd4e9875d774613bc9affc21943a7a9cc3b5
Cheryl Velez <onegrumpybunny@gmail.com>
2019-07-17 07:47:46 -0400

    Pushing App Files

---
+++ app/src/main/java/android/example/com/userprofile/MainActivity.java
@@ -0,0 +1,42 @@
+package android.example.com.userprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView nameView = (TextView) findViewById(R.id.name);
+        TextView birthdayView = (TextView) findViewById(R.id.birthday);
+        TextView countryView = (TextView) findViewById(R.id.country);
+
+        String name = "Cheryl";
+        String birthday = "August";
+        String country = "USA";
+
+        nameView.setText(name);
+        birthdayView.setText(birthday);
+        countryView.setText(country);
+
+        /** profile_picture1 is defined in XML
+         * profile_picture2 is defined with setImageResource
+         * profile_picture3 is defined with setDrawable
+         */
+
+        ImageView pic2 = (ImageView) findViewById(R.id.profile_picture2);
+        pic2.setImageResource(R.drawable.profilepic);
+
+        ImageView pic3 = (ImageView) findViewById(R.id.profile_picture3);
+        Drawable drawable = getResources().getDrawable(R.drawable.profilepic);
+        pic3.setImageDrawable(drawable);
+
+    }
+}
